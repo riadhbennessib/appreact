ae9fba5483b6f67bdc973bdf0388837114f6f421
riadh BEN NESSIB <riadhbennessib@gmail.com>
2020-08-15 18:56:00 +0100

    Updates

---
+++ precache-manifest.014b82c9ccb1825fffcd4aa69165d3e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd3b2056e73123dbf6b1833fd259bc7c",
+    "url": "/appreact/index.html"
+  },
+  {
+    "revision": "5d07536b66fa36139c98",
+    "url": "/appreact/static/css/main.e7903be2.chunk.css"
+  },
+  {
+    "revision": "c719d3a3121054f2346d",
+    "url": "/appreact/static/js/2.662041c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/appreact/static/js/2.662041c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d07536b66fa36139c98",
+    "url": "/appreact/static/js/main.6947354a.chunk.js"
+  },
+  {
+    "revision": "8a00efab10d763c590ac",
+    "url": "/appreact/static/js/runtime-main.3f0a1d91.js"
+  },
+  {
+    "revision": "4d114be07c5c8fc645ffa6f455fe7d4e",
+    "url": "/appreact/static/media/riadhbennessib.4d114be0.jfif"
+  }
+]);
